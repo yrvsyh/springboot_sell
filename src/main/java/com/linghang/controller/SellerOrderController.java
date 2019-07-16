9ecff7370cf5d6daa8dae08801ad712f06148533
yuan20180608 <202999054@qq.com>
2019-07-16 15:11:57 +0800

    删除空行

--- src/main/java/com/linghang/controller/SellerOrderController.java
+++ src/main/java/com/linghang/controller/SellerOrderController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.util.Map;
 
 @Slf4j
